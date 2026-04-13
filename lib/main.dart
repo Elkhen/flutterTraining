3852fb1a36ed48ca27f0fdf7382fcda07ba5db3f
Mateusz Kierończyk <elkhen@gmail.com>
2026-04-13 20:34:40 +0200

    flutter training part 2

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Tile('A', HitType.partial),
+        appBar: AppBar(
+          title: Align(alignment: Alignment.centerLeft, child: Text('Birdle'))
         ),
+        body: Center(child: GamePage()),
       ),
     );
   }
@@ -49,4 +50,32 @@ class Tile extends StatelessWidget {
     );
   }
 }
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This object is part of the game.dart file.
+  // It manages wordle logic, and is outside the scope of this tutorial.
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (var guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for (var letter in guess)
+                  Tile(letter.char, letter.type)
+              ],
+            ) 
+        ],
+      ),
+    );
+  }
+}
+
 
